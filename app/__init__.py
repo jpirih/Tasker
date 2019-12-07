ed01cbc4a1656f5af6818e26ba985e5da336ceb7
Janko Pirih <janko.pirih@gmail.com>
2019-12-07 18:56:19 +0100

    new app structure

--- app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from app.main import bp as main_bp
-
-
-app = Flask(__name__)
-app.register_blueprint(main_bp)
--- main/routes.py
+++ main/routes.py
@@ -1,4 +1,4 @@
-from app.main import bp
+from main import bp
 from flask import render_template
 
 
